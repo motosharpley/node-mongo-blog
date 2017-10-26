f347e24a879f16bf70105d603f04cc3b3d4a65a4
motosharpley <motosharpley@gmail.com>
2017-10-26 14:18:31 -0500

    add gitignore

--- app.js
+++ app.js
@@ -1,11 +1,94 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
-const app = express();
+mongoose.connect('mongodb://localhost/nodedb');
+let db = mongoose.connection;
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+// Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
 });
 
+// Check for db errors
+db.on('error', function(err){
+    console.log(err);
+});
+
+// Init App 
+const app = express();
+
+// Bring in Models
+let Article = require('./models/article');
+
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// body-parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function(req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root  = namespace.shift()
+        , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            vlaue : value
+        };
+    }
+}));
+
+// Home Route
+app.get('/', function(req, res){
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err);
+        } else {
+        res.render('index', {
+            title:'Articles',
+            articles: articles
+        });
+      }
+    });
+});
+
+// Route Files
+let articles = require('./routes/articles');
+app.use('/articles', articles);
+
+// Start Server
 app.listen(3000, function(){
     console.log('Server started on port 3000...');
 });
